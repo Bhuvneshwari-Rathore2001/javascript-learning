be01df4a35b02d92f64df579b320be788f48a31b
bhuvneshwari <bhuvneshwarirathore4@gmail.com>
2023-11-21 20:27:59 +0530

    modules changes

---
+++ module1.js
@@ -0,0 +1,12 @@
+const name = () => {
+     console.log("Hello Bhuvi")
+}
+
+
+const name2 = (namee) => {
+     console.log("Hello" + namee)
+}
+
+
+
+module.exports = {name,name2};
---
+++ modules.js
@@ -0,0 +1,16 @@
+
+
+// Common js
+
+
+// here name nd name2 after const by using destructuring
+
+
+// const {name,name2} = require("./module 1")
+import Aman, {name, name2} from "./module2.js"
+name()
+name2("Aman")
+name2("Shivani")
+name2("Garima")
+Aman()
+                                         
